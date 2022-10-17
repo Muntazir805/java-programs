c3cdfa7155898e8e091407d0f9711e060abea5b1
Muntazir Hussain Panhwar <115171519+Muntazir805@users.noreply.github.com>
2022-10-17 09:55:29 +0500

    Create CPN.java

---
+++ CPN.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class CPN {
+
+public static void main (String [ ] args){ Scanner input = new Scanner(System.in);
+float matric,inter,test;
+System.out.println("Enter matric marks");
+matric = input.nextInt();
+matric=matric/850100;
+System.out.println("Enter inter marks");
+inter = input.nextInt();
+inter=inter/1100100;
+System.out.println("Enter test marks");
+test = input.nextInt();
+test=test/100100;
+float CPN;
+matric = matric10/100;
+inter = inter30/100;
+test = test60/100;
+CPN =matric+inter+test;
+System.out.println("Total CPN is" +CPN);
+
+}
+}
+
