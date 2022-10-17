6e6572a148779f45dab6afa60c4792755549d26c
Muntazir Hussain Panhwar <115171519+Muntazir805@users.noreply.github.com>
2022-10-17 09:58:11 +0500

    Create Usingoperator.java

---
+++ Usingoperator.java
@@ -0,0 +1,21 @@
+import java.util.;
+class Usingoperator {
+public static void main(String[]args) { Scanner input = new Scanner(System.in);
+int number1,number2;
+float result;
+System.out.println("Enter a number");
+number1=input.nextInt();
+System.out.println("Enter Second number");
+number2=input.nextInt();
+result=number1+number2;
+System.out.println("Sum of two numbers is"+result);
+result=number1-number2;
+System.out.println("Subtraction of two numbers is"+result);
+result=number1number2;
+System.out.println("Multiplication of two numbers is"+result);
+result=number1/number2;
+System.out.println("Division of two numbers is "+ result);
+result=number1%number2;
+System.out.println("Reminder of a number is"+result);
+}						}
+}
