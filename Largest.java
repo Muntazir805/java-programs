ee7b3d909ee73e400d16c5b3bcbd351f06bb34fd
Muntazir Hussain Panhwar <115171519+Muntazir805@users.noreply.github.com>
2022-10-17 09:59:58 +0500

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class largest{
+public static void main(String[] args) { Scanner input = new Scanner(System.in);
+int number1 ,number2;
+System.out.println("Enter any number");
+number1 = input.nextInt();
+System.out.println("Enter Second number");
+number2 = input.nextInt();
+if(number1>number2){
+System.out.println( "The largest number is"+number1);}
+else
+System.out.println("The largest number is"+number2);
+}
+}
