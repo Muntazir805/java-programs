3697bf1346577b384f2b02b83ea1a831eb35f16f
Muntazir Hussain Panhwar <115171519+Muntazir805@users.noreply.github.com>
2022-10-17 09:52:12 +0500

    Create EvenoOdd.java

---
+++ EvenoOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class EvenOdd {
+public static void main(String[ ] args ) {
+Scanner input = new Scanner(System.in);
+System.out.println("Enter a Number");
+int num =input.nextInt();
+if (num%2==0)
+{
+System.out.print("Number is Even");
+}
+else
+System.out.print("Number is odd");
+}
+}
